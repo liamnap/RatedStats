5b8b305a96f45ec96c48f80ae2e5746a46e61b21
liamnap <122205142+liamnap@users.noreply.github.com>
2026-03-15 13:39:06 +0000

    bloodlust sound

--- frameanimation.lua
+++ frameanimation.lua
@@ -78,6 +78,6 @@ function RSTATS:PlayDailyMenuIntro(menu)
 
     menu.DailyIntro:SetAlpha(0)
     menu.DailyIntro:Show()
-    PlaySoundFile("Sound\\Spells\\bloodlust_player_cast_head.wav", "Master")
+    PlaySound(10030, "Master")
     menu.DailyIntro.anim:Play()
 end
